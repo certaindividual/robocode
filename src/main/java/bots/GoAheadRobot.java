05ed56880453e262bafcff089606d895ab1284db
Mateusz Monasterski <mateusz.mona@interia.pl>
2021-01-15 16:10:59 +0100

    Hello world bot

---
+++ src/main/java/bots/GoAheadRobot.java
@@ -0,0 +1,11 @@
+package bots;
+
+import robocode.Robot;
+
+public class GoAheadRobot extends robocode.Robot {
+    public void run() {
+        while(true){
+            ahead(100);
+        }
+    }
+}
